3568dbead13500fc0c9086f7bfe08c778959ef69
Gureum <67192959+huheyun@users.noreply.github.com>
2024-11-23 23:09:38 +0900

    Update MovieFetcher.java

--- Movie/MovieDataFetcher1/lib/src/main/java/_MovieDataFetcher1/MovieFetcher.java
+++ Movie/MovieDataFetcher1/lib/src/main/java/_MovieDataFetcher1/MovieFetcher.java
@@ -5,7 +5,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class MovieFetcher {
-    private static final String API_KEY = "73e437a2cc6f065cbaf58c32d011f598";
+    private static final String API_KEY = "YOUR_API_KEY";
     private static final String API_URL = "https://api.themoviedb.org/3/discover/movie";
 
     public static String fetchMovies() throws Exception {
